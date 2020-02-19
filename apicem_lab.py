c39df7ed01734364ba62b09e83d2a0b5432289bf
sagorbd91 <sagor.ghosh@dal.ca>
2020-02-19 08:04:37 -0400

    got 200

--- apicem_lab.py
+++ apicem_lab.py
@@ -3,6 +3,13 @@ import json
 from pprint import pprint
 apicem_ip = "https://sandboxapicem.cisco.com"
 credentials = {"username":"devnetuser", "password":"Cisco123!"}
-version = "V1"
+version = "v1"
 # https://sandboxapicem.cisco.com/api/v1/ticket
-ticket_url = apicem_ip
+ticket_url = apicem_ip+ "/api/" + version + "/ticket"
+headers = {"content-type":"application/json"}
+response = requests.post(url=ticket_url, headers = headers, 
+                         data = json.dumps(credentials),
+                         verify = True)
+
+print(response.status_code)
+r_json = response.json()
