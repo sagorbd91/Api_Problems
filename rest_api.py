73a7f7172b846a1585c3e5ddcda670950d4496fa
sagorbd91 <sagor.ghosh@dal.ca>
2020-02-19 07:43:24 -0400

    openweather problem

---
+++ rest_api.py
@@ -0,0 +1,44 @@
+# List
+"""
+trainer = ["ben finkel", "creamy ciorra"]
+thingsilove = [trainer, "chocolat", "chicken", 12]
+print(thingsilove)"""
+
+# Dictionary
+"""
+sagor = {"name": "ghosh", "age": 54, "favourite": "chokolat"}
+print(sagor)"""
+"""
+favcisco = ["show ip int brief", "show run", "show run | include router interfaces"]
+
+sagor = {"name": "ghosh", "age": 54, "favourite": "chokolat", "commands": favcisco}
+
+print(sagor["commands"])"""
+
+#openweather_api key = 1dfdaa0233fae87f58e47ba2796f12d3
+
+import requests
+from pprint import pprint
+import json
+
+
+hostname = 'http://api.openweathermap.org'
+resource = '/data/2.5/weather'
+url = hostname + resource + '?q=Halifax&units=metric&appid=1dfdaa0233fae87f58e47ba2796f12d3'
+response = requests.get(url)
+print(response)
+weather = response.json()
+print(type(weather))
+weather_str = json.dumps(weather, indent=2) # json dumps use for converting string
+#print(weather_str)
+#print(weather["main"]["temp"])
+print(weather["weather"][0]["description"])
+#print(weather["clouds"]["all"])
+
+
+cred = {"username": "sagor.ghosh@dal.ca", "password": "December@25"}
+url = 'http://api.openweathermap.org'
+ticket_url
+
+
+
