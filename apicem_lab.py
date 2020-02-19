4d9a8093606e2b364efc50b0f0f420d05dcb36a2
sagorbd91 <sagor.ghosh@dal.ca>
2020-02-19 08:19:55 -0400

    username has found

--- apicem_lab.py
+++ apicem_lab.py
@@ -5,7 +5,7 @@ apicem_ip = "https://sandboxapicem.cisco.com"
 credentials = {"username":"devnetuser", "password":"Cisco123!"}
 version = "v1"
 # https://sandboxapicem.cisco.com/api/v1/ticket
-ticket_url = apicem_ip+ "/api/" + version + "/ticket"
+ticket_url = apicem_ip+ "/api/" + version + "/ticket" # made ticket through variable
 headers = {"content-type":"application/json"}
 response = requests.post(url=ticket_url, headers = headers, 
                          data = json.dumps(credentials),
@@ -13,3 +13,26 @@ response = requests.post(url=ticket_url, headers = headers,
 
 print(response.status_code)
 r_json = response.json()
+pprint(r_json)
+
+ticket = r_json['response']['serviceTicket'] # get service ticket from the response
+print(ticket)
+
+user_url = apicem_ip + "/api/" + version + "/user"
+headers = {"content-type":"application/json", 
+            "X-Auth-Token": ticket}
+
+response = requests.get(url=user_url, headers = headers)
+
+print(response.status_code)
+r_json = response.json()
+pprint(r_json)
+
+username = r_json['response'][0]['username']
+print(username)
+
+
+
+
+
+
